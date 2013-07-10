3835c733d2a8d48bec2b7bfa4c1489b34120069f
josselinchevalay <josselin54.chevalay@gmail.com>
2013-07-10 21:35:18 +0200

    modification model
    rectification du model

--- Core/model.php
+++ Core/model.php
@@ -11,22 +11,52 @@
 * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 * Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*	@class model
-*   @comment : classe mère de tout les model
+*	
+*  
 *  @version 0.0.1 
+*  @package MiniVc
 */
 class Model{
+
+	/**
+	* 
+	* <p>
+	*	table
+	* </p>
+	*/
 	public $table; // nom de la table
+
+
+	/**
+	* 
+	* <p>
+	*	id
+	* </p>
+	*/
 	public $id;   // id -> doit être reporter en réel sur la table en bdd
+	
+
+	/**
+	*
+	* <p>
+	*	db
+	* </p>
+	*/
 	public $db;   // dd type objet contient Le datasotre
 
 	
-	public function __construct(){
+	/**
+	* <p> constructeur </p>
+	*/
+	public function __construct(){       
 		$this->db = DataStore::getInstance()->getDb();
 	}
 	
 
-	// méthode qui va permettre de recherche un objet par son id
+	/**
+	*
+	* <p> read</p>
+	*/
 	public function read($fields = null){		
 		if($fields == null){ $fields="*";}
 		$sql = "SELECT ".$fields." FROM ".$this->table." WHERE id=".$this->id;		
@@ -38,7 +68,10 @@ class Model{
 		}
 	}
 
-	// méthode qui update ou persit la classe
+	/**
+	* <p> save /<p>
+	*
+	*/
 	public function save($data){
 		if(isset($data['id']) && !empty($data['id'])){
 			$sql = "UPDATE ".$this->table." SET ";
@@ -63,7 +96,7 @@ class Model{
 			$sql = substr($sql, 0, -1);
 			$sql .=")";			
 		}
-		echo $sql;
+		
 		$req= $this->db->query($sql);
 		if(!isset($data['id'])){
 			$this->id =mysql_insert_id();
@@ -73,15 +106,18 @@ class Model{
 	}
 
 
-	// fonction qui permet la recherche
+	/**
+	* <p> find </p>
+	*
+	*/
 	public function find($data =  array()){
 		if(isset($data['conditions'])){$conditions = $data['conditions'];}else{$conditions= null;}
-		if(isset($data['fields'])){$fields = $data['fields'];}else{$fields [] =  array("*");}
+		if(isset($data['fields'])){$fields = $data['fields'];}else{$fields =  array("*");}
 		if(isset($data["limit"])){$limit = " LIMIT ".$data['limit'];}else{$limit= "";}
 		if(isset($data["order"])){$order = " ORDER BY ".$data['order'];}else{$order = null;}
-		
 		$sql = "SELECT ";
 		foreach ($fields as $key => $value) {
+
 			$sql .= "$value ,";
 		}
 		$sql = substr($sql, 0, -1);
@@ -96,7 +132,7 @@ class Model{
 		if(!empty($limit)){
 			$sql .= $limit;
 		}
-		
+
 		$dataSet =  array();
 		$req = $this->db->query($sql);
 		foreach ($req as $o => $occurence) {
@@ -105,14 +141,21 @@ class Model{
 		return $dataSet;
 	}
 
-	// méthode qui permet de supprimer
+	/**
+	* <p> delete </p>
+	*
+	*/
 	public function delete($id = null){
 		if($id == null){$id = $this->id;}
 		$sql = "DELETE FROM ".$this->table." WHERE id =".$id;		
 		$this->db->query($sql);
 	}
 
-	// fonction qui mount un modele
+	/**
+	*
+	* <p>load</p>
+	*
+	*/
 	static function load($name){
 		require($name.".php");
 		return new $name();
